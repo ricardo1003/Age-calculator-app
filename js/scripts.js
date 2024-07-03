9c006e7502dc6fc10e516f18c59a9eea8f7d90f5
unknown <ricardodonado1105@gmail.com>
2024-07-02 21:43:36 -0500

    calculator logic created

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
-console.log("hello world!")
+const presentDay = (new Date()).getDay()
+const presentMonth = (new Date()).getMonth()+1
+const presentYear = (new Date()).getFullYear()
+
+let calculatedYears
+let calculatedMonths
+let calculatedDays
+
+function calculateAge(day, month, year){
+    let years = presentYear - year
+    let months = presentMonth - month
+    let days =  presentDay - day
+
+    if(days < 0){
+        months--
+        days = new Date(presentYear, presentMonth - 1, 0).getDate() + days
+    }
+    if(months < 0){
+        years--
+        months = 12 + months 
+    }
+
+    calculatedDays = days
+    calculatedMonths = months
+    calculatedYears = years
+}
+
+calculateAge(11,5,2008)
+
+console.log(`${calculatedYears} years, ${calculatedMonths} months, ${calculatedDays} days.`)
+
